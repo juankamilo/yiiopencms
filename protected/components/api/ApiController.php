b1dc3a04bc283ec2f664ab6feb9567635ac46685
juankamilo <juankaka@gmail.com>
2014-07-31 08:32:14 -0500

    Actualizacion Get Started

--- protected/components/api/ApiController.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Controller is the customized base controller class.
- * All controller classes for this application should extend from this base class.
- */
-class ApiController extends CController
-{
-   
-}
